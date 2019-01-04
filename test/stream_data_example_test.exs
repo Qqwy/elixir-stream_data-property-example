4fe07d0f591325298f78dd4ecf7160f55126a876
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-04 16:31:20 +0100

    Tests that compile

--- test/stream_data_example_test.exs
+++ test/stream_data_example_test.exs
@@ -6,20 +6,22 @@ defmodule StreamDataExampleTest do
   # describe "halve_even" do
   property "halve_even" do
     call = fn num ->
+      ## In real code, there would be more set-up code here ...
       StreamDataExample.halve_even(%{difficult_to_construct: %{number: num}})
+      ## ... and potentially extra code here to transform the result in something 'check-able'.
     end
 
-
     # property "halves on small even numbers" do
-      check all num <- integer(max: 10_000), Integer.is_even(num) do
+      check all num <- integer(0..10_000), Integer.is_even(num) do
         result = call.(num)
         assert {:ok, div(num, 2)} == result
       end
     # end
 
     # property "returns error on too big errors" do
-      check all num <- integer(min: 10_000), Integer.is_even(num) do
-        assert {:error, _} = call.(num)
+      check all num <- integer(),
+        Integer.is_even(num) do
+        assert {:error, _} = call.(abs(num) + 10_000)
       end
     # end
 
