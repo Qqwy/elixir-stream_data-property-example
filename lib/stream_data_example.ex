95d6fdca0a85b55dd072106d830072e73533cecd
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-01-04 16:11:57 +0100

    test example

--- lib/stream_data_example.ex
+++ lib/stream_data_example.ex
@@ -1,18 +1,19 @@
 defmodule StreamDataExample do
-  @moduledoc """
-  Documentation for StreamDataExample.
-  """
+  require Integer
 
-  @doc """
-  Hello world.
+  def halve_even(%{difficult_to_construct: %{number: even_number}}) when Integer.is_even(even_number) do
+    if even_number < 10_000 do
+      {:ok, div(even_number, 2)}
+    else
+      {:error, :number_too_big}
+    end
+  end
 
-  ## Examples
+  def halve_even(%{difficult_to_construct: %{number: number}}) when not Integer.is_even(number) do
+    {:error, :not_an_even_number}
+  end
 
-      iex> StreamDataExample.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def halve_even(_other) do
+    {:error, :improperly_constructed}
   end
 end
--- test/stream_data_example_test.exs
+++ test/stream_data_example_test.exs
@@ -1,8 +1,37 @@
 defmodule StreamDataExampleTest do
+  require Integer
   use ExUnit.Case
-  doctest StreamDataExample
+  use ExUnitProperties
 
-  test "greets the world" do
-    assert StreamDataExample.hello() == :world
+  # describe "halve_even" do
+  property "halve_even" do
+    call = fn num ->
+      StreamDataExample.halve_even(%{difficult_to_construct: %{number: num}})
+    end
+
+
+    # property "halves on small even numbers" do
+      check all num <- integer(max: 10_000), Integer.is_even(num) do
+        result = call.(num)
+        assert {:ok, div(num, 2)} == result
+      end
+    # end
+
+    # property "returns error on too big errors" do
+      check all num <- integer(min: 10_000), Integer.is_even(num) do
+        assert {:error, _} = call.(num)
+      end
+    # end
+
+    # property "Returns error on non-even numbers" do
+      check all num <- integer(), Integer.is_odd(num) do
+        assert {:error, _} = call.(num)
+      end
+    # end
+    # property "Returns error non-numeric inputs" do
+      check all thing <- any(), not(is_integer(thing)) do
+        assert {:error, _} = call.(num)
+      end
+    # end
   end
 end
